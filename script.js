a7993576d59c93eaf460ff9656388e3bde1610d8
tnjcode <fachrialkayyis@gmail.com>
2024-08-02 15:39:31 +0700

    AI

--- script.js
+++ script.js
@@ -1,27 +1,59 @@
 let video = document.getElementById("video");
 let canvas = document.body.appendChild(document.createElement("canvas"));
-let ctx = canvas.getContext("2d")
+let ctx = canvas.getContext("2d");
 
-let width = 1280;
+let width = 1080;
 let height = 720;
 
-const startSteam = () => {
+const startStream = () => {
     console.log("------start stream------");
     navigator.mediaDevices.getUserMedia({
-        video : {width, height},
-        audio : false
-    }).then((stream) => {video.srcObject = stream});  
-}
+        video: { width, height },
+        audio: false
+    }).then((stream) => { video.srcObject = stream });
+};
 
 console.log(faceapi.nets);
 
-console.log("------start load models------");
+console.log("----- START LOAD MODEL ------");
 Promise.all([
     faceapi.nets.ageGenderNet.loadFromUri('models'),
-    faceapi.nets.ssdMobilenetv1.loadFromDisk('./models'),
+    faceapi.nets.ssdMobilenetv1.loadFromUri('models'),
     faceapi.nets.tinyFaceDetector.loadFromUri('models'),
-    faceapi.nets.facelandmark68Net.loadFromUri('models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('models'),
     faceapi.nets.faceRecognitionNet.loadFromUri('models'),
     faceapi.nets.faceExpressionNet.loadFromUri('models')
-]).then(startSteam);
+]).then(startStream);
 
+async function detect() {
+    const detections = await faceapi.detectAllFaces(video)
+                                        .withFaceLandmarks()
+                                        .withFaceExpressions()
+                                        .withAgeAndGender();
+    console.log(detections);
+    
+    ctx.clearRect(0,0, width, height);
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    // draw detections into the canvas
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+    console.log(resizedDetections);
+    
+    resizedDetections.forEach(result => {
+        const {age, gender, genderProbability} = result;
+        new faceapi.draw.DrawTextField ([
+            `${Math.round(age,0)} Tahun`,
+            `${gender} ${Math.round(genderProbability)}`
+        ],
+        result.detection.box.bottomRight
+        ).draw(canvas);
+    });
+}
+
+
+video.addEventListener('play', () => {
+    displaySize = { width, height };
+    faceapi.matchDimensions(canvas, displaySize);
+    setInterval(detect, 100);
+});
